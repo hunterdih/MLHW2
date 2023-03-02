946320cb2d87453c9ebd09f155e2c7ded9629bb8
David Hunter <hunter.davi@northeastern.edu>
2023-03-02 08:39:39 -0500

    Updates

--- Problem3.py
+++ Problem3.py
@@ -2,68 +2,79 @@ import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+
 CONTOUR_LEVELS = np.geomspace(0.0001, 250, 100)
 MARKER_TOTAL = 4
-NOISE_SIGMA = 0.3
-SIGMAX = 0.9
-SIGMAY = 0.9
-SIG_ARRAY = np.array([[pow(SIGMAX,2),0],[0,pow(SIGMAY,2)]])
-LEVELS = np.geomspace(1, 300, 100)
+NOISE_SIGMA = 0.01
+SIGMAX = 0.5
+SIGMAY = 0.5
+SIG_ARRAY = np.array([[pow(SIGMAX, 2), 0], [0, pow(SIGMAY, 2)]])
+LEVELS = np.geomspace(0.001, 5000, 300)
+GRID_RESOLUTION = 256
+GRID_HORIZONTAL = 20
+GRID_VERTICAL = 20
+CIRCLE_RADIUS = 5
+RANDOM_THETA = True
 
-random.seed(3)
+#random.seed(3)
 if __name__ == '__main__':
     # Initialize the true position of the vehicle
-    r = random.random()
+    r = random.uniform(0, CIRCLE_RADIUS)
     theta = random.uniform(0, 2)
     x = r * np.cos(theta * np.pi)
     y = r * np.sin(theta * np.pi)
     xy_t = np.array((x, y))
 
-    # Generate the locations of the landmarks
+    # Generate the locations list of the landmarks
     markers = np.zeros((MARKER_TOTAL, 2))
     for i in range(MARKER_TOTAL):
-        theta = (2 * np.pi) / MARKER_TOTAL * i
-        x = np.cos(theta)
-        y = np.sin(theta)
-        markers[i,:] = np.array((x,y))
-        distance_from_true = []
-        for marker in markers[:i+1]:
-            distance_from_true.append(np.linalg.norm(xy_t-marker))
+        # Markers are assumed to be equidistant from one another, can also generate along random points if other portion is un-commented
+        if RANDOM_THETA:
+            theta = (2 * random.uniform(0, 2))
+        else:
+            theta = (2 * np.pi) / MARKER_TOTAL * i
 
-        # Need to generate a coordinate field. For this example, 512x512
-        grid_orig = np.meshgrid(np.linspace(-2, 2, 512), np.linspace(-2, 2, 512))
-        grid = np.expand_dims(np.transpose(grid_orig, axes=(1,2,0)), axis=len(np.shape(grid_orig))-1)
+        x = CIRCLE_RADIUS * np.cos(theta)
+        y = CIRCLE_RADIUS * np.sin(theta)
+        markers[i, :] = np.array((x, y))
+        distance_from_true = []
+        for marker in markers[:i + 1]:
+            # Add a distance measurement for each marker in play, distance is equal to the magnitude of |[xT yT] - [xi yi]|
+            distance_from_true.append(np.linalg.norm(xy_t - marker))
+
+        # Need to generate a coordinate field. For this example, 512x512. Larger coordinate fields improve accuracy (possible use case for drone capstone)
+        grid_orig = np.meshgrid(np.linspace(-GRID_HORIZONTAL, GRID_HORIZONTAL, GRID_RESOLUTION), np.linspace(-GRID_VERTICAL, GRID_VERTICAL, GRID_RESOLUTION))
+        grid = np.expand_dims(np.transpose(grid_orig, axes=(1, 2, 0)), axis=len(np.shape(grid_orig)) - 1)
 
         # Equation 28 in report, first portion
         # If the grid was not resized, np.swapaxes() would not be used and a transpose would work instead
-        pO = np.matmul(np.matmul(grid, np.linalg.inv(SIG_ARRAY)), np.swapaxes(grid, 2, 3))[:,0,0,0]
+        pO = np.matmul(np.matmul(grid, np.linalg.inv(SIG_ARRAY)), np.swapaxes(grid, 2, 3))[:, 0, 0, 0]
 
         # Need to calculate the second portion (sum) of equation 28 in report
         range = []
         for marker_index, distance in enumerate(distance_from_true):
+            # Need to expand markers[marker_index] dimensionality
             grid_distance = np.linalg.norm(grid - markers[marker_index][None, None, None, :], axis=3)
-            range.append(np.squeeze(pow(distance-grid_distance,2)/pow(NOISE_SIGMA,2)))
+            range.append(np.squeeze(pow(distance - grid_distance, 2) / pow(NOISE_SIGMA, 2)))
         range = sum(range)
-        param_estimate_grid = pO+range
+        param_estimate_grid = pO + range
 
         # Plot Findings
 
         fig0 = plt.figure(0, figsize=[10, 10])
         ax = fig0.add_subplot(1, 1, 1)
-        circ = plt.Circle((0, 0), radius=1, edgecolor='r', facecolor='None')
+        circ = plt.Circle((0, 0), radius=CIRCLE_RADIUS, edgecolor='r', facecolor='None')
         ax.scatter(xy_t[0], xy_t[1], 100, marker='+')
-        color_list = ['g', 'b', 'y', 'm', 'k', 'c','g', 'b', 'y', 'm', 'k', 'c','g', 'b', 'y', 'm', 'k', 'c','g', 'b', 'y', 'm', 'k', 'c']
+        color_list = ['g', 'b', 'y', 'm', 'k', 'c', 'g', 'b', 'y', 'm', 'k', 'c', 'g', 'b', 'y', 'm', 'k', 'c', 'g', 'b', 'y', 'm', 'k', 'c']
         for marker_index, distance in enumerate(distance_from_true):
             xy = markers[marker_index]
-            ax.scatter(xy[0], xy[1], 100, marker = 'o', color = color_list[marker_index])
-            ax.add_patch(plt.Circle((xy[0],xy[1]), radius=distance, edgecolor=color_list[marker_index], facecolor='None'))
-        ax.set_xlim((-2,2))
-        ax.set_ylim((-2,2))
-        plt.contour(grid_orig[0], grid_orig[1], param_estimate_grid, levels = CONTOUR_LEVELS)
+            ax.scatter(xy[0], xy[1], 100, marker='o', color=color_list[marker_index])
+            ax.add_patch(plt.Circle((xy[0], xy[1]), radius=distance, edgecolor=color_list[marker_index], facecolor='None'))
+        ax.set_xlim((-GRID_HORIZONTAL, GRID_HORIZONTAL))
+        ax.set_ylim((-GRID_VERTICAL, GRID_VERTICAL))
+        plt.contour(grid_orig[0], grid_orig[1], param_estimate_grid, levels=LEVELS)
 
         ax.add_patch(circ)
         plt.show()
         print(f'{r=}')
         print(f'{theta=} * Pi')
-
-
